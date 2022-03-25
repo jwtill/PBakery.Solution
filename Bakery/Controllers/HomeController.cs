f3a9a87d88d5b88f1ea135a5ef1974a31077203a
Josh Tillinghast <jwtill@icloud.com>
2022-03-25 11:21:33 -0700

    WIP work on splash page to display all treats and flavors

--- Bakery/Controllers/HomeController.cs
+++ Bakery/Controllers/HomeController.cs
@@ -1,12 +1,26 @@
+using Bakery.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Bakery.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly BakeryContext _db;
+    public HomeController(BakeryContext db)
+    {
+      _db = db;
+    }
     [HttpGet("/")]
     public ActionResult Index()
     {
+      ViewBag.Treats = _db.Treats.ToList();
+      ViewBag.Flavors = _db.Flavors.ToList();
+      ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "Name");
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
       return View();
     }
   }
